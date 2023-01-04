97418806f2b82d954db23af4275b88f682a2327f
Matthias Wolf <matze.wolf@tum.de>
2023-01-04 17:49:58 +0100

    Initial commit

---
+++ pages/new_report.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.markdown("""
+    # Neue Meldung
+
+    > Bitte beachte, dass dieses Tool keine Rechtsberatung ersetzt.
+    > Bitte konsultiere immer eine qualifizierte Anwältin oder einen qualifizierten Anwalt.
+    > Wir helfen dir dabei am Ende dieses Fragebogens.
+""")

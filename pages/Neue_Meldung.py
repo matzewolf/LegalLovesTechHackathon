727e4e6b56cd3c85d91550db35bc3f3e3202dbfe
Matthias Wolf <matze.wolf@tum.de>
2023-01-04 19:33:41 +0100

    form completed

---
+++ pages/Neue_Meldung.py
@@ -0,0 +1,58 @@
+import streamlit as st
+
+st.markdown("""
+    # Neue Meldung
+
+    > Bitte beachte, dass dieses Tool keine Rechtsberatung ersetzt.
+    > Bitte konsultiere immer eine qualifizierte Anwältin oder einen qualifizierten Anwalt.
+    > Wir helfen dir dabei am Ende dieses Fragebogens.
+""")
+
+result = None
+
+provable = st.selectbox(
+    "Kann man die Aussage beweisen oder widerlegen?",
+    ["", "Ja", "Nein"]
+)
+if provable == "Ja":
+    others = st.selectbox(
+        "Wurde die Aussage nur vor dir oder auch vor einer oder mehrerer anderer Personen getätigt?",
+        ["", "Nur vor mir", "Auch vor einer oder mehrerer anderer Personen"]
+    )
+    if others == "Nur vor mir":
+        true_expression = st.selectbox(
+            "Ist die Aussage im Prinzip wahr, aber abwertend?",
+            ["", "Ja", "Nein"]
+        )
+        if true_expression == "Ja":
+            result = "eine Formalbeleidigung"
+        elif true_expression == "Nein":
+            result = "eine Beleidigung"
+    elif others == "Auch vor einer oder mehrerer anderer Personen":
+        false_expression = st.selectbox(
+            "Ist die Aussage beweisbar unwahr?",
+            ["", "Ja", "Nein"]
+        )
+        if false_expression == "Ja":
+            result = "Verleumdung"
+        elif false_expression == "Nein":
+            result = "Üble Nachrede"
+elif provable == "Nein":
+    judging = st.selectbox(
+        "Wertet dich die Aussage als Person herab?",
+        ["", "Ja", "Nein"]
+    )
+    if judging == "Ja":
+        result = "eine Beleidigung"
+    elif judging == "Nein":
+        reputation = st.selectbox(
+            "Schadet die Aussage deiner Reputation?",
+            ["", "Ja", "Nein"]
+        )
+        if reputation == "Ja":
+            result = "Verleumdung"
+        elif reputation == "Nein":
+            result = "keinen Straftatsbestand"
+
+if result:
+    st.write(f"In diesem Fall handelt es sich wahrscheinlich um {result}.")
--- pages/new_report.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.markdown("""
-    # Neue Meldung
-
-    > Bitte beachte, dass dieses Tool keine Rechtsberatung ersetzt.
-    > Bitte konsultiere immer eine qualifizierte Anwältin oder einen qualifizierten Anwalt.
-    > Wir helfen dir dabei am Ende dieses Fragebogens.
-""")

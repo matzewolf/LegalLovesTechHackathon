a527c4311280d280499a62035df5ad19754c1e11
Matthias Wolf <matze.wolf@tum.de>
2023-01-07 00:54:02 +0100

    Added contacts

--- pages/3_Ansprechpartner.py
+++ pages/3_Ansprechpartner.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_card import card
 
 
 st.set_page_config(
@@ -11,12 +12,25 @@ st.sidebar.image("assets/advotis_small.png")
 st.markdown("""
     # Ansprechpartner
 """)
-
-st.selectbox("Filter", [
-    "Meine Ansprechpartner",
-    "Spezialisierte Anwälte",
-    "Beratungsstellen",
-    "Örtliche Polizeibehörden",
-    "Staatsanwaltschaft",
-    "Amtsgericht",
-])
+st.components.v1.html("""
+    <div data-anw-widget="217fe44880e867"></div>
+    <script async type="text/javascript" src="https://widget.anwalt.de/suche/21/baseColor/000000/buttonColor/e95d0f/headerColor/b3b3b3/width/400/rounded/1/linksNewWindow/1/disableBorder/0/get.js?uid=7fe44880e867&v=2"></script>
+""", height=500)
+card(
+    title="HateAid",
+    text="Beratungsstelle für Betroffene digitaler Gewalt",
+    image="https://i.ibb.co/rp4SQ45/hateaid.png",
+    url="https://hateaid.org/betroffenenberatung/",
+)
+card(
+    title="Polizei",
+    text="Onlinewachen der Polizeien der Länder",
+    image="https://i.ibb.co/8xL4L8b/polizei.jpg",
+    url="https://www.polizei.de/Polizei/DE/Einrichtungen/Onlinewache/onlinewache_node.html",
+)
+card(
+    title="Gerichte und Staatsanwaltschaft",
+    text="Orts- und Gerichtsverzeichnis",
+    image="https://i.ibb.co/LhqyYJT/justitia.jpg",
+    url="https://www.justizadressen.nrw.de/de/justiz/suche",
+)

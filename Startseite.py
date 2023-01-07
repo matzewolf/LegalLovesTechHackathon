1500670e6be9816b0b9af893b5fc810b2c030516
Matthias Wolf <matze.wolf@tum.de>
2023-01-07 16:29:06 +0100

    add feedback form

--- Startseite.py
+++ Startseite.py
@@ -12,22 +12,27 @@ st.sidebar.image("assets/advotis_small.png")
 st.image("assets/advotis_small.png")
 st.title("Startseite")
 page_1 = st.button(
-    ":heavy_plus_sign: &nbsp;&nbsp; Neuen Bericht erstellen",
+    ":heavy_plus_sign: &nbsp;&nbsp; Neuen Bericht erstellen"
 )
 if page_1:
     switch_page("Neuen_Bericht_erstellen")
 page_2 = st.button(
-    ":arrows_counterclockwise: &nbsp;&nbsp; Verlauf",
+    ":arrows_counterclockwise: &nbsp;&nbsp; Verlauf"
 )
 if page_2:
     switch_page("Verlauf")
 page_3 = st.button(
-    ":busts_in_silhouette: &nbsp;&nbsp; Ansprechpartner",
+    ":busts_in_silhouette: &nbsp;&nbsp; Ansprechpartner"
 )
 if page_3:
     switch_page("Ansprechpartner")
 page_4 = st.button(
-    ":book: &nbsp;&nbsp; Gesetzestexte",
+    ":book: &nbsp;&nbsp; Gesetzestexte"
 )
 if page_4:
     switch_page("Gesetzestexte")
+page_5 = st.button(
+    ":bulb: &nbsp;&nbsp; Feedback"
+)
+if page_5:
+    switch_page("Feedback")
---
+++ pages/5_Feedback.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="advotis – Feedback",
+    page_icon="assets/advotis_small.png",
+    layout="centered",
+)
+st.sidebar.image("assets/advotis_small.png")
+
+st.components.v1.html("""
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfMPPTQSXGc3KH7hXIiK7sH5lmKFlWoI4VrPJyDTQK55ijTfw/viewform?embedded=true" width="640" height="835" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+""",
+height=835)
